1d2ebc75317821fd11b98c4d3d30222c52c43781
Nishizawa Eiji <eieioeiji0501@gmail.com>
2021-10-22 19:01:04 +0900

    update main program

--- scripts/main.py
+++ scripts/main.py
@@ -7,17 +7,37 @@ import pygame
 from pygame.locals import *
 
 ### 定数
+D_SIZE_X = 400
+D_SIZE_Y = 640
+USER_POS = 40
+USER_HP  = 100
 BAL_SIZE = 18       # ボールサイズ
 F_RATE   = 60       # フレームレート
 K_REPEAT = 20       # キーリピート発生間隔
-USER_SPD  = 10      # ユーザー移動速度
+USER_SPD = 10       # ユーザー移動速度
+USER_X   = 600      #
 BAL_SPD  = 10       # ボール移動速度
 F_SIZE   = 60       # フォントサイズ
 S_TIME   = 2        # START画面時間(秒)
 E_TIME   = 4        # CLEAR画面時間(秒)
 
 ### 画面定義(X軸,Y軸,横,縦)
-SURFACE  = Rect(0, 0, 400, 640) # 画面サイズ
+SURFACE  = Rect(0, 0, D_SIZE_X, D_SIZE_Y) # 画面サイズ
+
+class Timer:
+    def __init__(self):
+        self.first=True
+        self.start=pygame.time.get_ticks()
+    def reset(self):
+        self.first=True
+    def stand_by(self,t):#[ms]
+        if self.first:
+            self.start=pygame.time.get_ticks()
+            self.first=False
+        if (pygame.time.get_ticks()-self.start)>=t:
+            self.first=True
+            return True
+        return False
 
 class User(pygame.sprite.Sprite):
     def __init__(self,surface,ini,color):
@@ -25,58 +45,86 @@ class User(pygame.sprite.Sprite):
         self.ini=ini
         self.color=color
         self.x=self.ini[0]
+        self.Hit=0
+        self.hp=USER_HP
+        self.hpflag=False
+        self.t1=Timer()
+        self.t2=Timer()
     def x(self,x):
         self.x=x
     def return_x(self):
         return self.x
     def addx(self,add):
         self.x+=add
-    def update(self,t):
+    def Hp(self):
+        return self.hp
+    def hit(self,num):
+        self.Hit=num
+        self.hpflag=False
+    def update(self):
         if self.x<=0:
             self.x=0
-        elif self.x>=400:
+        elif self.x>=D_SIZE_X:
             self.x=400
-        pygame.draw.circle(self.surface,self.color, (self.x,self.ini[1]), 10)
+        if self.Hit>0:
+            if not self.hpflag:
+                self.hp-=self.Hit
+                self.hpflag=True
+            if self.t2.stand_by(1000):
+                pygame.draw.circle(self.surface,self.color, (self.x,self.ini[1]), 10)
+            if self.t1.stand_by(5000):
+                self.t2.reset()
+                self.t1.reset()
+                self.Hit=0
+        else:
+            pygame.draw.circle(self.surface,self.color, (self.x,self.ini[1]), 10)
         return
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self,surface,ini,color):
+    def __init__(self,surface,ini,color,shootsp):
         self.surface=surface
         self.ini=ini
         self.color=color
         self.x=self.ini[0]
         self.y=self.ini[1]
-        self.shootsp=0
-        self.shootFlag=False
-    def shoot(self,x,shootsp):
         self.shootsp=shootsp
-        self.x=x
-        self.y=self.ini[1]
-        self.shootFlag=True
-    def update(self,t):
-        if self.shootFlag:
-            self.y+=self.shootsp
-            if self.y<=0 or self.y>=640:
-                self.y=self.ini[1]
-                self.shootFlag=False
-            else:
-                pygame.draw.ellipse(self.surface,self.color, (self.x,self.y,5,20,))
+        self.Hit=False
+        self.Range=20
+    def hit(self):
+        return self.Hit
+    def update(self,par_x):
+        self.y+=self.shootsp
+        if self.shootsp>0:
+            h=self.y>=D_SIZE_Y-USER_POS
+        elif self.shootsp<0:
+            h=self.y<=USER_POS
+        if par_x-self.Range < self.x and self.x < par_x+self.Range and h:
+            self.Hit=True
+            return True
+        if self.y<=0 or self.y>=D_SIZE_Y:
+            self.y=self.ini[1]
+            return True
+        else:
+            pygame.draw.ellipse(self.surface,self.color, (self.x,self.y,5,20,))
+        return False
 
 ############################
 ### メイン関数
 ############################
 def main():
     t=0
+    bulletflag=False
     ### 画面初期化
     pygame.init()
     surface = pygame.display.set_mode(SURFACE.size)
     ### 時間オブジェクト生成
     clock = pygame.time.Clock()
     ### Object
+    t1=Timer()
     user1=User(surface,[200,600],[255,0,0])
     user2=User(surface,[200,40],[0,0,255])
-    bullet1=Bullet(surface,[200,600],[255,255,0])
-    bullet2=Bullet(surface,[200,40],[0,255,255])
+    bullet1=[]
+    bullet2=[]
     ### キーリピート有効
     pygame.key.set_repeat(K_REPEAT)
     ### STARTを表示
@@ -94,13 +142,27 @@ def main():
         ### 背景色設定
         surface.fill((0,0,0))
         ###描画処理
-        bullet1.update(t)
-        bullet2.update(t)
-        user1.update(t)
-        user2.update(t)
+        count=0
+        for bullet in bullet1:
+            if bullet.hit():
+                count+=1
+            if(bullet.update(user2.return_x())):
+                del bullet1[bullet1.index(bullet)]
+        user2.hit(count)
+        count=0
+        for bullet in bullet2:
+            if bullet.hit():
+                count+=1
+            if(bullet.update(user1.return_x())):
+                del bullet2[bullet2.index(bullet)]
+        user1.hit(count)
+        user1.update()
+        user2.update()
         ### 画面更新
         pygame.display.update()
         ### イベント処理
+        if t1.stand_by(800):#チャタリング防止
+            bulletflag=False
         for event in pygame.event.get():
             ### 終了処理
             if event.type == QUIT:
@@ -110,12 +172,17 @@ def main():
                     exit()
                 ### キー操作
                 if event.key == K_LEFT:
-                    user1.addx(USER_SPD)
-                if event.key == K_RIGHT:
                     user1.addx(-USER_SPD)
+                if event.key == K_RIGHT:
+                    user1.addx(USER_SPD)
                 if event.key == K_SPACE:
-                    bullet1.shoot(user1.return_x(),-10)
-
+                    if not bulletflag:
+                        bullet1.append(Bullet(surface,[user1.return_x(),D_SIZE_Y-USER_POS],[255,255,0],-10))
+                        bulletflag=True
+                if event.key == K_LCTRL:
+                    if not bulletflag:
+                        bullet2.append(Bullet(surface,[user2.return_x(),USER_POS],[0,255,255],10))
+                        bulletflag=True
 ############################
 ### 終了関数
 ############################
