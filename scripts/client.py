8346acfe15c0341e2cc5b0ef12b1d843433729ea
umezawa <m17219@g.metro-cit.ac.jp>
2021-11-08 10:50:34 +0900

    update client and server

--- scripts/client.py
+++ scripts/client.py
@@ -1,6 +1,23 @@
 import socket
 
-def communication(ip,data):
-	print(ip,data)
-	read=[0,200,False]
+
+N = 256
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect(ip)
+	port = 8000
+	s.connect((ip, port))
+
+def communication(data):
+	senddata = str(data[0]) + "," + str(data[1]) + "," +str(data[2])
+	s.send(senddata.encode("utf-8"))
+	data = s.recv(N)
+	temp = data.decode("utf-8")
+	temp = temp.split(",")
+	print(data)
+	read=[int(temp[0]),int(temp[1]),bool(temp[2])]
 	return read
+
+
+
+
--- scripts/server.py
+++ scripts/server.py
@@ -1,3 +1,6 @@
+import socket
+import threading
+import time
 
 first=True
 save_id=0
@@ -9,3 +12,38 @@ def identification(id):
     if id==save_id:
         return True
     return False
+
+
+PORT = 8000
+N = 256
+clientno = 0
+resive1 = ""
+resive2 = ""	
+
+def send_data(client, clientno):
+	data = ("1")
+	resive = client.recv(N)
+	temp = resive.decode("utf-8")
+	temp = temp.split(",")
+	print(resive)
+	temp = int(temp[0])
+	print(temp)
+	resive1 = ("10".encode("utf-8"))
+	resive2 = ("20".encode("utf-8"))
+	if identification(temp):
+		resive1 = resive
+		client.send(resive2)#データ送信
+	else:
+		
+		resive2 = resive
+		client.send(resive1)
+	client.close()
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(("",PORT))
+server.listen(0)
+
+while True:
+	client, addr = server.accept()
+	p = threading.Thread(target = send_data, args = (client, clientno))
+	p.start()
