c566fef1433b894cb19b341fa7f0e1f78a0cf37a
Nishizawa Eiji <eieioeiji0501@gmail.com>
2021-11-15 13:06:27 +0900

    update client

--- scripts/client.py
+++ scripts/client.py
@@ -40,6 +40,12 @@ def Conect(ip):
 	return s
 
 def communication(s,data):
+	###送信
+	senddata = str(data[0]) + "," + str(data[1]) + "," +str(int(data[2])) + "," + str(data[3])
+	try:
+		s.send(senddata.encode("utf-8"))
+	except:
+		pass
 	###受信
 	data = s.recv(N)
 	###受信データ変換
@@ -50,13 +56,6 @@ def communication(s,data):
 		read=[int(temp[0]),int(temp[1]),strtobool(temp[2]),int(temp[3])]
 	except:
 		read=[0,200,False]
-	###送信
-	senddata = str(data[0]) + "," + str(data[1]) + "," +str(int(data[2])) + "," + str(data[3])
-	try:
-		s.send(senddata.encode("utf-8"))
-	except:
-		pass
-
 	return read
 
 
